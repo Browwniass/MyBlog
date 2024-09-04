8259969f5127a8a8d95215cf2d967b95fd5d9b0b
Browwniass <95659478+Browwniass@users.noreply.github.com>
2024-09-04 21:51:25 +0300

    add pagination

--- project_blog/project_blog/settings.py
+++ project_blog/project_blog/settings.py
@@ -116,7 +116,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'project_blog.permissions.IsAdminOrReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 30   
 
 }
 
