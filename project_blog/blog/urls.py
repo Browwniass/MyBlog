21c728c1498a9e8ab3dd54d5ea3d7227e877f80b
Browwniass <95659478+Browwniass@users.noreply.github.com>
2024-09-04 17:43:02 +0300

    add Logout

--- project_blog/blog/urls.py
+++ project_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import ArticleModelView, Logout
+from .views import ArticleModelView, Logout, TagsModelView
 from rest_framework.authtoken import views
 
 
--- project_blog/blog/views.py
+++ project_blog/blog/views.py
@@ -4,7 +4,7 @@ from .models import Article
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from taggit.models import Tag
 
 class ArticleModelView(viewsets.ModelViewSet):
     queryset = Article.objects.all()
